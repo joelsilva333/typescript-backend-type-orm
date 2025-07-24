901a5f0e315708d4f6d34031220fb5d7b2a47798
joelsilva333 <joelpitra44@gmail.com>
2025-07-24 21:45:27 +0100

    Atualiza configuração do Docker, adiciona entidade Product e ajusta dependências no package.json

---
+++ src/entities/product.entity.ts
@@ -0,0 +1,30 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm"
+import { v4 as uuid } from "uuid"
+
+@Entity("product")
+export class Product {
+	@PrimaryColumn()
+	id: string
+
+	@Column()
+	name: string
+
+	@Column()
+	description: string
+
+	@Column()
+	weight: number
+
+	@CreateDateColumn({
+		name: "created_at",
+		type: "timestamp",
+		default: () => "CURRENT_TIMESTAMP",
+	})
+	createdAt: Date
+
+	constructor() {
+		if (!this.id) {
+			this.id = uuid()
+		}
+	}
+}
