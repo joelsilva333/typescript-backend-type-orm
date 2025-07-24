b679979e9a096e8bc7b2d7ba0cb6a857be36573f
Joel Silva <joelpitra44@gmail.com>
2025-07-24 16:03:52 +0100

    Formata o código para seguir convenções de estilo e melhorar a legibilidade

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
-import dotenv from "dotenv"
-import express from "express"
-import './connection'
+import dotenv from "dotenv";
+import express from "express";
+import "./connection";
 
-dotenv.config()
-const PORT = process.env.PORT || 8080
-const app = express()
+dotenv.config();
+const PORT = process.env.PORT || 8080;
+const app = express();
 
 app.listen(PORT, () => {
-	console.log(`Server is running on port ${PORT}`)
-})
+  console.log(`Server is running on port ${PORT}`);
+});
 
 app.get("/", (req, res) => {
-	res.send("Hello, World!")
-})
+  res.send("Hello, World!");
+});
