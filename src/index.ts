6b8e7fd79a6fe054d4b0c3b1b475a6078d3c9177
joelsilva333 <joelpitra44@gmail.com>
2025-08-04 22:51:22 +0100

    remove espaços em branco desnecessários no arquivo index.ts

--- src/index.ts
+++ src/index.ts
@@ -14,15 +14,13 @@ dotenv.config()
 const PORT = process.env.PORT || 8080
 
 const app = express()
+
 app.use(cors())
 app.use(routes)
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 
 app.listen(PORT, () => {
 	console.log(`Swagger docs: http://localhost:${PORT}/api-docs`)
 })
-
-
